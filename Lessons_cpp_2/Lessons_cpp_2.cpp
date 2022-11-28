d809172cd4d958c688727cc4d83b74888f21eb1e
M.Silin <yaroslavasiiliina@gmail.com>
2022-11-28 15:37:46 +0300

    аапапаапп

--- Lessons_cpp_2/Lessons_cpp_2.cpp
+++ Lessons_cpp_2/Lessons_cpp_2.cpp
@@ -1,1 +1,19 @@
-﻿
+﻿#include <iostream> 
+using namespace std;
+
+int* Sum(int a, int b) {
+	int* dinamic = new int;
+	*dinamic = a + b;
+	return dinamic;
+}
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	int* barbecue = Sum(a, b);
+	cout << *barbecue << endl;
+
+	delete barbecue;
+
+	return 0;
+}
