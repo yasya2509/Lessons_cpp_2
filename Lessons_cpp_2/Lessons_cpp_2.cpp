6488c791cda786d192e1b29280a58310a155af62
M.Silin <yaroslavasiiliina@gmail.com>
2022-12-05 15:09:53 +0300

    1ф. (создала динамический массив, заполнила его нулями) 2ф. (вывод массива) main собрали все воедино мы молодцы!

--- Lessons_cpp_2/Lessons_cpp_2.cpp
+++ Lessons_cpp_2/Lessons_cpp_2.cpp
@@ -1,18 +1,33 @@
 ﻿#include <iostream> 
+
 using namespace std;
 
+// создается динамический массив :)
+int* Creater(int size)
+{
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = 0;
+	}
+	return arr;
+}
+
+void Print(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
 
 int main()
 {
 	int size = 5;
 	cin >> size;
-	int* arr = new int [size];
-	for (int i = 0; i < size; i++)
-	{
-		arr[i]= 0;
-		cout << arr[i] << endl;
-	}
-	delete [] arr;
+	int* ARR = Creater(size);
+	Print(ARR, size);
+	delete[] ARR;
 	return 0;
 }
 
