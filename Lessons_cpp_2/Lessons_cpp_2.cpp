1b37e339070a7f202878d654db43bfe581bbab10
M.Silin <yaroslavasiiliina@gmail.com>
2022-12-07 17:52:22 +0300

    нвая версия неправильного дз

--- Lessons_cpp_2/Lessons_cpp_2.cpp
+++ Lessons_cpp_2/Lessons_cpp_2.cpp
@@ -1,8 +1,56 @@
 ﻿#include <iostream> 
 using namespace std;
 
+int** FillTArr(int str, int stolb)
+{
+    int** arr = new int* [str];
+    for (int i = 0; i < str; ++i)
+    {
+        arr[i] = new int[stolb];
+    }
+    return arr;
+}
+
+void Print(int** arr, int str, int stolb)
+{
+    for (int i = 0; i < str; ++i)
+    {
+        for (int j = 0; j < stolb; ++j)
+        {
+            cout << arr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
+int** SumArr(int str, int stolb, int size_1, int size_2, int** arr_first, int** arr_second)
+{
+    int** arr = FillTArr(str, stolb);
+    for (int i = 0; i < size_1; ++i) 
+    {
+        for (int j = 0; j < size_2; ++j)
+        {
+
+           arr[i][j] = arr_first[i][j] + arr_second[i][j];
+            cout << arr << ' ';
+        }
+        cout << endl;
+    }
+    return arr;
+}
+
+void DelArr(int** arr, int str, int stolb)
+{
+    for (int i = 0; i < str; ++i) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+}
+
 int main()
 {
+    int str = 5;
+    int stolb = 7;
     const int size_1 = 2;
     const int size_2 = 2;
  
@@ -15,16 +63,9 @@ int main()
         {8,1}
     };
 
-   
-
-    for (int i = 0; i < size_1; ++i) {
-        for (int j = 0; j < size_2; ++j) {
-
-            arr_first[i][j] += arr_second[i][j];
-            cout << arr_first[i][j] << ' ';
-        }
-        cout << endl;
-    } 
+    int** sum = SumArr(str, stolb, size_1, size_2, arr_first, arr_second);
+    Print(sum, str, stolb);
+    DelArr(sum, str, stolb);
 
 	return 0;
 }
