f0a8a540a5f184277c3f7ba15e5b3ba919515e5c
M.Silin <yaroslavasiiliina@gmail.com>
2022-12-12 15:21:35 +0300

    Тестирование. Факториал.

--- Lessons_cpp_2/Lessons_cpp_2.cpp
+++ Lessons_cpp_2/Lessons_cpp_2.cpp
@@ -1,90 +1,32 @@
 ﻿#include <iostream> 
+#include <cassert> 
+
 using namespace std;
 
-int** CreateArr(int str, int stolb)
-{
-    int** arr = new int* [str];
-    for (int i = 0; i < str; ++i)
-    {
-        arr[i] = new int[stolb];
-    }
-    return arr;
-}
+int Factorial(int value) {
+	if (value < 0) {
+		return 0;
+	}
 
-void Print(int** arr, int str, int stolb)
-{
-    for (int i = 0; i < str; ++i)
-    {
-        for (int j = 0; j < stolb; ++j)
-        {
-            cout << arr[i][j] << ' ';
-        }
-        cout << endl;
-    }
-}
-
-int** SumArr(int str, int stolb, int** dinamic_arr_first, int** dinamic_arr_second)
-{
-    int** arr = CreateArr(str, stolb);
-    for (int i = 0; i < str; ++i)
-    {
-        for (int j = 0; j < stolb; ++j)
-        {
-            arr[i][j] = dinamic_arr_first[i][j] + dinamic_arr_second[i][j];
-            
-        }
-    }
-    return arr;
-}
-
-void DelArr(int** arr, int str, int stolb)
-{
-    for (int i = 0; i < str; ++i) {
-        delete[] arr[i];
-    }
-
-    delete[] arr;
-}
-
-int main()
-{
-    // посмотри внимательнее на переменные ниже
-    const int str = 2;
-    const int stolb = 4;
-    //const int size_1 = 2;
+	int fact = 1;
+	for (int i = 1; i <= value; i++) {
+		fact *= i;
+	}
     
-    int arr_first[str][stolb] = {
-        {1,2,3,5},
-        {2,9,6,7}
+	return fact;
+}
 
-    };
-    int arr_second[str][stolb] = {
-        {9,8,7,5},
-        {8,1,4,3}
-    };
-    
+void Test() {
+	assert(Factorial(3) == 6);
+	assert(Factorial(1) == 1);
+	assert(Factorial(0) == 1);
+	assert(Factorial(-2) == 0);
 
-    // Это делается для того, чтобы передать матрицу в функцию
-    int** dinamic_arr_first = CreateArr(str, stolb);
-    for (int i = 0; i < str; ++i) {
-        for (int j = 0; j < stolb; ++j) {
-            dinamic_arr_first[i][j] = arr_first[i][j];
-        }
-    }
-    int** dinamic_arr_second = CreateArr(str, stolb);
-    for (int i = 0; i < str; ++i) {
-        for (int j = 0; j < stolb; ++j) {
-            dinamic_arr_second[i][j] = arr_second[i][j];
-        }
-    }
+}
 
-    // и вот тут подумай какие из переменных тебе не нужно передавать
-    int** sum = SumArr(str, stolb, dinamic_arr_first, dinamic_arr_second);
-    Print(sum, str, stolb);
-    DelArr(sum, str, stolb);
+int main() {
 
-    DelArr(dinamic_arr_first, str, stolb);
-    DelArr(dinamic_arr_second, str, stolb);
+	Test();
 
-    return 0;
-}  
+	return 0;
+}
