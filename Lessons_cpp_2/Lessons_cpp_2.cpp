623d0b566b9244286864f3d2530f5f753137ad5e
M.Silin <yaroslavasiiliina@gmail.com>
2022-12-27 12:25:43 +0300

    искатель искомого элемента в строке

--- Lessons_cpp_2/Lessons_cpp_2.cpp
+++ Lessons_cpp_2/Lessons_cpp_2.cpp
@@ -6,42 +6,39 @@
 
 using namespace std;
 
-void Sort(const vector<int>& elements, vector<int>& chet, vector<int>& no_chet) {
-    for (size_t i = 0; i < elements.size(); ++i) {
-        if (elements[i] % 2 == 0)
+// symbol - искомый символ в строке
+string FindSymbol(const string& text, char symbol)
+{
+    string box;
+    for (size_t i = 0; i < text.size(); ++i)
+    {
+        if (text[i] == symbol)
         {
-            chet.push_back(elements[i]);
+            box.push_back(text[i]);
         }
-        else
-        {
-            no_chet.push_back(elements[i]);
-        }
-       
     }
-    
-}
-
-void Print(const vector<int>& elements) {
-    for (size_t i = 0; i < elements.size(); ++i) {
-        cout << elements[i] << endl;
-    }
+    // составляет строку из искомых символов
+    return box;
 }
 
 void Test() {
-    vector<int> elements = { 12,5,8,65,3,23,7,599,5,2,35,35,6 };
-    vector<int> chet;
-    vector<int> no_chet;
-    Sort(elements, chet, no_chet);
-    assert(chet.size() == 4);
-    assert(no_chet.size() == 9);
+    string text = "aosgpenamllaskhksjdfaahrda"s;
+    {
+        string result = FindSymbol(text, 'a');
+        assert(result.size() == 6);
+        assert(result == "aaaaaa"s);
+    }
+    {
+        string result = FindSymbol(text, 's');
+        assert(result.size() == 3);
+        assert(result == "sss"s);
+    }
 }
 
 int main() {
     Test();
-    vector<int> elements = { 12,5,8,65,3,23,7,599,5,2,35,35,6 };
-    vector<int> chet;
-    vector<int> no_chet;
-    Sort(elements, chet, no_chet);
-    Print(no_chet);
+    string text = "aosgpenamllaskhksjdfaahrda"s;
+    string result = FindSymbol(text, 'a');
+    cout << result << endl;
     return 0;
 }
