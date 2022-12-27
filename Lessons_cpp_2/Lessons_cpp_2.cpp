a5bd151904acdc0046677a2ccb6504b80e30cce5
M.Silin <yaroslavasiiliina@gmail.com>
2022-12-27 11:05:37 +0300

    начало

--- Lessons_cpp_2/Lessons_cpp_2.cpp
+++ Lessons_cpp_2/Lessons_cpp_2.cpp
@@ -1,48 +1,28 @@
-﻿#include <iostream> 
-#include <cassert> 
-#include <vector>
-using namespace std;
-int SumElements(vector<int> third) 
-{
-	int sum_v_third = 0;
-	for (size_t i = 0; i < third.size(); ++i) {
-		sum_v_third += third[i];
-			//cout << third[i] << endl;
+﻿/**
 
-	}
-	return sum_v_third;
+***/
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void Sort(const vector<int>& elements, vector<int>& chet, vector<int>& no_chet) {
+    // одним циклом разделяет четные и нечетные по разным векторам
 }
 
 void Test() {
-	vector<int> third = { 10,10,10,5 };
-	assert(SumElements(third) == 35);
+    vector<int> elements = { 12,5,8,65,3,23,7,599,5,2,35,35,6 };
+    vector<int> chet;
+    vector<int> no_chet;
+    Sort(elements, chet, no_chet);
+    assert(chet.size() == 4);
+    assert(no_chet.size() == 9);
 }
 
-int main()
-{
-	Test();
-	vector<int> first = { 1,2,3 };
-	vector<int> second = { 9,8,7 };
-	vector<int> third;
+int main() {
 
-	for (size_t i = 0; i < first.size(); ++i) {
-		third.push_back(first[i] + second[i]);
 
-	}
-
-	for (size_t i = 0; i < third.size(); ++i) {
-		cout << third[i] << ' ';
-	}
-	cout << endl;
-
-	third.push_back(5);
-	cout << third.size() << endl;
-	for (size_t i = 0; i < third.size(); ++i) {
-		cout << third[i] << ' ';
-	}
-	cout << endl;
-
-	cout << SumElements(third);
-	
-	return 0;
+    return 0;
 }
